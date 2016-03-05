6487651cbf1609fce5f41808369806dcb8eab9f2
daerokos <daerokos@gmail.com>
2016-03-04 23:55:02 -0800

    Added files via upload

---
+++ LPTHWEX43/gfpp25.py
@@ -0,0 +1,9 @@
+# 1. Write about or draw the problem
+# 2. Extract key concepts from #1 and research them
+# 3. Create a class hierarchy and object map for the concepts
+# 4. Code the classes and a test to run them
+# 5. Repeat and refine
+
+###GOTHONS FROM PLANET PERCAL #25###
+from sys import exit
+from random import randint
